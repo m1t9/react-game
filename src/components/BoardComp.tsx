b67cbdb766190c3c8bc6797147869441b3051dbd
m1t9 <dpleshanov@gmail.com>
2021-03-02 03:59:18 +0300

    feat: status/style

--- src/components/BoardComp.tsx
+++ src/components/BoardComp.tsx
@@ -7,11 +7,13 @@ import { Board } from '../model/Board';
 const board: Board = new Board();
 
 let currnetPlayer: boolean = true;
+// let status: string = `Current player: X`
 
 interface ICell {
   value: string | number,
   x: number,
   y: number,
+  // onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 interface IState {
@@ -30,32 +32,36 @@ class Cell extends React.Component<ICell, IState> {
       y: props.y,
       aviable: true,
     }
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
   // clickHandler() {
     
   // }
+  clickHandler() {
+    if (this.state.aviable && board.checkWinner().figure === 'n') {
+      const figure: string = currnetPlayer ? 'X' : 'O';
+      this.setState({value: figure});
+      board.setItem(this.state.x, this.state.y, figure);
+      // console.log(board.checkWinner());
+      currnetPlayer = !currnetPlayer;
+      this.setState({aviable: !this.state.aviable});
+      // console.log(this);
+      
+      // if (board.checkWinner().figure !== 'n') alert(`Winner: ${board.checkWinner().figure}`);
+    } else {
+      return;
+    }
+  }
 
   render() {
+    const completed: string = board.checkWinner().figure === 'n' ? 'cell' : 'cell completed ex4'; 
     return (
       <div
-        className="cell"
-        key={this.state.x * CONSTANTS.FIELD_SIZE + this.state.y}
-        onClick={() => {
-          if (this.state.aviable && board.checkWinner().figure === 'n') {
-            const figure: string = currnetPlayer ? 'X' : 'O';
-            this.setState({value: figure});
-            board.setItem(this.state.x, this.state.y, figure);
-            // console.log(board.checkWinner());
-            currnetPlayer = !currnetPlayer;
-            this.setState({aviable: !this.state.aviable});
-            
-            if (board.checkWinner().figure !== 'n') alert(`Winner: ${board.checkWinner().figure}`);
-          } else {
-            return;
-          }
-          
-        }}
+        className={`${this.state.aviable ? completed : 'cell insert'}`}
+        // className="cell"
+        // key={this.state.x * CONSTANTS.FIELD_SIZE + this.state.y}
+        onClick={this.clickHandler}
       >
         {this.state.value}
       </div>
@@ -63,11 +69,21 @@ class Cell extends React.Component<ICell, IState> {
   }
 }
 
-export class BoardComp extends React.Component {
+export class BoardComp extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      status: `Current player: X`,
+      isNext: true,
+    }
+    this.changeFigureState = this.changeFigureState.bind(this);
+  }
+
   repBoard: any[] = [];
 
   renderCell(value: number, x: number, y: number) {
-    return <Cell value={value} x={x} y={y} />
+    // return <Cell value={value} x={x} y={y} onClick={() => {console.log('wow')}}/>
+    return <Cell value={value} x={x} y={y}/>
   }
 
   createBoard(): any[] {
@@ -85,15 +101,32 @@ export class BoardComp extends React.Component {
     return res;
   }
 
+  changeFigureState() {
+    this.setState({isNext: !this.state.isNext});
+  }
+
   render() {
+    // currnetPlayer ? this.setState({status: 'X'}) : this.setState({status: 'O'});
+    // this.setState({status: 'O'});
+    let status: string;
+    const winner = board.checkWinner().figure;
+    if (winner !== 'n') {
+      status = `Winner: ${winner}`
+    } else {
+      status = this.state.isNext ? 'Current player: X' : 'Current player: O';
+    }
     
     const res = this.createBoard();
 
     return (
-      <div
-        key="board"
-        className="board">
-        {res}
+      <div>
+        <div className="status">{status}</div>
+        <div
+          onClick={this.changeFigureState}
+          key="board"
+          className="board">
+          {res}
+        </div>
       </div>
     )
   }
